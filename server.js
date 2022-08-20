4a86cb4e3573b7dbfc79c60c5a53da603de8170d
xiooon <99613028+xiooon@users.noreply.github.com>
2022-08-20 18:46:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const port = 3000;
 
 app.use(express.json());
 app.use(cors());
@@ -42,3 +43,4 @@ app.get("/review/:reviewid/:restaurantid", (req, res) => {
 });
 
 app.listen(port, () => console.log('Example app listening on port ${port}!'))
+});
